cf00f64b3e47202ab020ce8eba69ec5328eeb4bb
chowdhury-yeamin <chowdhuryyeamin07@gmail.com>
2025-11-01 10:58:00 +0600

    Added my own firebase

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRzbErh_TkSoaSQg-xrDqutqvcm0tpswo",
-  authDomain: "smart-deals-6768c.firebaseapp.com",
-  projectId: "smart-deals-6768c",
-  storageBucket: "smart-deals-6768c.firebasestorage.app",
-  messagingSenderId: "954044857203",
-  appId: "1:954044857203:web:15cda9eff195bb88e0a306"
+  apiKey: "AIzaSyChyksasIVJGAum74V3Y6SYLUmZWufh5nc",
+  authDomain: "smart-deals-1d09a.firebaseapp.com",
+  projectId: "smart-deals-1d09a",
+  storageBucket: "smart-deals-1d09a.firebasestorage.app",
+  messagingSenderId: "732541717632",
+  appId: "1:732541717632:web:9a4aaeef561dce25caaa65",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
